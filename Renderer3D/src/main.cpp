dd6042ff6ed9397e5e500f94d4a866e4d6da5fc9
kubapoke <q.paczka@gmail.com>
2025-01-22 20:37:04 +0100

    modelling setup

--- Renderer3D/src/main.cpp
+++ Renderer3D/src/main.cpp
@@ -1,9 +1,9 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
-#include "stb_image/stb_image.h"
-#include "Shader/Shader.h"
-#include "Camera/Camera.h"
+#include "stb_image.h"
+#include "Shader.h"
+#include "Camera.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
