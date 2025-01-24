62e18ca14ec83a85f2c998d0b7ac5366f3b895c9
kubapoke <q.paczka@gmail.com>
2025-01-24 18:08:05 +0100

    changed shader input/output format

--- Renderer3D/assets/shaders/BaseFragmentShader.fs
+++ Renderer3D/assets/shaders/BaseFragmentShader.fs
@@ -19,9 +19,11 @@ struct PointLight {
     float quadratic;
 };
 
-in vec3 FragPos;
-in vec3 Normal;
-in vec2 TexCoords;
+in VS_OUT{
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
 
 uniform vec3 viewPos;
 uniform Material material;
@@ -30,22 +32,22 @@ uniform PointLight pointLight;
 void main()
 {
     // ambient
-    vec3 ambient = pointLight.ambient * texture(material.diffuse1, TexCoords).rgb;
+    vec3 ambient = pointLight.ambient * texture(material.diffuse1, fs_in.TexCoords).rgb;
 
     // diffuse
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(pointLight.position - FragPos);
+    vec3 norm = normalize(fs_in.Normal);
+    vec3 lightDir = normalize(pointLight.position - fs_in.FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = pointLight.diffuse * diff * texture(material.diffuse1, TexCoords).rgb;
+    vec3 diffuse = pointLight.diffuse * diff * texture(material.diffuse1, fs_in.TexCoords).rgb;
 
     // specular
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = pointLight.specular * spec * texture(material.specular1, TexCoords).rgb;
+    vec3 specular = pointLight.specular * spec * texture(material.specular1, fs_in.TexCoords).rgb;
 
     // attenuation
-    float distance    = length(pointLight.position - FragPos);
+    float distance    = length(pointLight.position - fs_in.FragPos);
     float attenuation = 1.0 / (pointLight.constant + pointLight.linear * distance + pointLight.quadratic * (distance * distance));
 
     ambient  *= attenuation;
